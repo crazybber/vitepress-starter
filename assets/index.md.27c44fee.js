d224729fddacfa438b70d3f6db5f19ec0d60da04
crazybber <crazybber@users.noreply.github.com>
2022-08-15 11:11:13 +0000

    deploy: 7f236b1edf09527aaa1f5ff3005a90482a053775

--- assets/index.md.27c44fee.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o,a}from"./app.d1b73383.js";const u='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}',r={},s=a("p",null,"#Hello world",-1),n=[s];function c(_,d,i,l,p,f){return o(),t("div",null,n)}var h=e(r,[["render",c]]);export{u as __pageData,h as default};
